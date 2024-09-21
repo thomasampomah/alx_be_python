6dd74155649bbb12f6ca899e821ef054222b1227
thomasampomah <ampomathomas1991@gmail.com>
2024-09-21 15:32:53 +0100

    advance task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = int(input('Enter your monthly income :'))
+monthly_expenses = int(input("“Enter your total monthly expenses:"))
+monthly_savings = monthly_income - monthly_expenses 
+Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)   
+print(monthly_savings)
+print(Projected_Savings)    
