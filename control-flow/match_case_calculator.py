ff301cdfe8cd00dc0612f7fd970d03e0d32b104e
thomasampomah <ampomathomas1991@gmail.com>
2024-09-26 17:40:15 +0100

    added task1

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,31 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input ("Choose the operation (+, -, *, /): ") 
+addition = num1 + num2 
+subraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2 
+
+match operation:
+    case "+":
+        print("The result is", addition)
+    case "-":
+        print("The result is", subraction)
+    case "*":
+        print("The result is", multiplication)
+    case "/": 
+        print("The result is", division)
+    case _:
+        print("Input correct operation") 
+    
+
+
+
+
+
+    
+
+
+    
+
+ 
