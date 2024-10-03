13fe44aea46d85de6e0dc3e8cb5c52ab0ca753d5
thomasampomah <ampomathomas1991@gmail.com>
2024-10-03 18:09:38 +0100

    file_added)

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,18 @@
 
 def perform_operation (num1, num2, operation):
-    z = num1+num2
-    print(z)
-perform_operation (1, 2, "+" )
+    add = z = num1 + num2
+    return z
+    substract = y = num1 - num2
+    return y
+    multiply = x = num1 * num2
+    return x
+    divide = j = num1 / num2
+    if num2 > 0:
+        print(j)
+    else:
+        print("Not divisible by zero" )
+
+
+
+
     
