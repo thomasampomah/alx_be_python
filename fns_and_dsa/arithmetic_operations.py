6e2921c3a01be8925dad375535f9feb129b067ac
thomasampomah <ampomathomas1991@gmail.com>
2024-10-03 18:44:17 +0100

    task0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,17 @@
 
-def perform_operation (num1, num2, operation):
+def perform_operation(num1, num2, operation):
     "add" = z = num1 + num2
     return z
 
-def perform_operation (num1, num2, operation):
+def perform_operation(num1, num2, operation):
     "substract" = y = num1 - num2
     return y
 
-def perform_operation (num1, num2, operation):
+def perform_operation(num1, num2, operation):
     "multiply" = x = num1 * num2
     return x
 
-def perform_operation (num1, num2, operation):
+def perform_operation(num1, num2, operation):
     "divide" = j = num1 / num2
     if num2 > 0:
         print(j)
