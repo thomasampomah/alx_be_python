b4782b5acce148caa1ccb544a92a153cc5261028
thomasampomah <ampomathomas1991@gmail.com>
2024-10-18 12:11:45 +0000

    taskcorrection

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,7 +5,7 @@ class BankAccount:
     def deposit(self,amount):
         if amount > 0:
             self.account_balance += amount
-            print(f"Deposited: ${amount:.1f}")
+            print(f"Deposited ${amount:.1f}")
         else:
             print("Deposit amount must be positive.")
 
