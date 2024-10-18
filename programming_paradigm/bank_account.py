59d4f31c30bda6f6e17202b147be3699eca665bb
thomasampomah <ampomathomas1991@gmail.com>
2024-10-18 11:01:20 +0000

    task0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,7 @@ class BankAccount:
 
     def deposit(self,amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount:.1f}")
+        print(f"Deposited: ${amount:.2f}")
     
     def withdraw(self,amount):
         self.account_balance -= amount
