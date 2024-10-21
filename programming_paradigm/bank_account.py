131298fa523c43f5e6a9370512f4a1603cffb800
thomasampomah <ampomathomas1991@gmail.com>
2024-10-21 20:58:34 +0000

    taskcorrection

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,7 +16,6 @@ class BankAccount:
             self.account_balance -= amount
             return True
         else:
-            print("Insufficient funds.")
             return False 
             
     
