0817254325e9743277074f5e50ebdf4601cdb673
thomasampomah <ampomathomas1991@gmail.com>
2024-10-23 18:55:02 +0000

    task2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,6 +11,9 @@ class TestCalc(unittest.TestCase):
 
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
+        
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
 
     def test_division(self):
         self.assertEqual(self.calc.divide(6, 3), 2)
