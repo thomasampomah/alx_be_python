c429765c341b82134e9b03f2a61913b46ed80385
thomasampomah <ampomathomas1991@gmail.com>
2024-10-23 18:47:40 +0000

    task2

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,19 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestCalc(unittest.TestCase):
+   
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(6, 3), 2)
+
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(10, 0)
+
+
+        
